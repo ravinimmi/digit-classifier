38807f3d2f507373b00790a1596b26f01000a5e4
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2016-05-26 23:33:35 +0530

    using panda for csv files

--- classifier.py
+++ classifier.py
@@ -1,47 +1,32 @@
 import csv
 import numpy as np
+import pandas as pd
 from sklearn import linear_model
 
-def read_data():
-    csvfile = open('train.csv')
-    csvreader = csv.reader(csvfile)
-    rows = []
-    for row in csvreader:
-        rows.append(row)
-    csvfile.close()
-    rows = np.array(rows)
-    XY = rows[1:,:]
-    XY = XY.astype(int)
-    X = XY[:,1:]
-    Y = XY[:,:1]
+def read_training_data():
+    train = pd.read_csv('train.csv')
+    YX = train.values
+    X = YX[:,1:]
+    Y = np.ravel(YX[:,:1])
     return X, Y
 
 def read_test_data():
-    csvfile = open('test.csv')
-    csvreader = csv.reader(csvfile)
-    rows = []
-    for row in csvreader:
-        rows.append(row)
-    csvfile.close()
-    rows = np.array(rows)
-    X_test = rows[1:,:]
-    X_test = X_test.astype(int)
+    test = pd.read_csv('test.csv')
+    X_test = test.values
     return X_test
 
 def generate_submission(X, Y, X_test):
-    Y = np.ravel(Y)
     logreg = linear_model.LogisticRegression()
     print "Training..."
     logreg.fit(X, Y)
     print "Predicting..."
     Y_predicted = logreg.predict(X_test)
-    submission_file = open('submission.csv', 'w')
-    csvwriter = csv.writer(submission_file)
-    csvwriter.writerow(['ImageId', 'Label'])
-    for i in range(len(Y_predicted)):
-        csvwriter.writerow([i+1, Y_predicted[i]])
-    submission_file.close()
+    df = pd.DataFrame({
+                    'ImageId': range(1, len(X_test)+1), 
+                    'Label'  : Y_predicted
+                })
+    df.to_csv('submission.csv', index=False)
 
-X, Y = read_data()
+X, Y = read_training_data()
 X_test = read_test_data()
 generate_submission(X, Y, X_test)
--- model_selection.py
+++ model_selection.py
@@ -1,31 +1,22 @@
 import csv
 import numpy as np
+import pandas as pd
 from sklearn import linear_model
 
 def read_data():
-    csvfile = open('train.csv')
-    csvreader = csv.reader(csvfile)
-    rows = []
-    for row in csvreader:
-        rows.append(row)
-    csvfile.close()
-    rows = np.array(rows)
-    XY = rows[1:,:]
-    XY = XY.astype(int)
-    X = XY[:,1:]
-    Y = XY[:,:1]
-    return X, Y
+    train = pd.read_csv('train.csv')
+    YX = train.values
+    return YX
 
-def split_dataset(X, Y):
-    XY = np.hstack((X, Y))
-    np.random.shuffle(XY)
-    N = len(XY)
-    XY_train = XY[:N*8/10, :]
-    XY_test  = XY[N*8/10:, :]
-    X = XY_train[:, :-1]
-    Y = np.ravel(XY_train[:, -1:])
-    X_test = XY_test[:, :-1]
-    Y_test = np.ravel(XY_test[:, -1:])
+def split_dataset(YX):
+    np.random.shuffle(YX)
+    N = len(YX)
+    YX_train = YX[:N*8/10, :]
+    YX_test  = YX[N*8/10:, :]
+    X = YX_train[:, 1:]
+    Y = np.ravel(YX_train[:, :1])
+    X_test = YX_test[:, 1:]
+    Y_test = np.ravel(YX_test[:, :1])
     return X, Y, X_test, Y_test
     
 def classify(X, Y, X_test, Y_test):
@@ -37,8 +28,7 @@ def classify(X, Y, X_test, Y_test):
     matches = (Y_predicted == Y_test)
     print "Accuracy = {accuracy:.2f}%".format(accuracy=100*sum(matches)/len(X_test))
 
-X, Y = read_data()
-X = X[:1000]
-Y = Y[:1000]
-X, Y, X_test, Y_test = split_dataset(X, Y)
+YX = read_data()
+# YX = YX[:1000]
+X, Y, X_test, Y_test = split_dataset(YX)
 classify(X, Y, X_test, Y_test)
